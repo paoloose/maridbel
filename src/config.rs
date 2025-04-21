384821356b043f9a5414d158237ef8e37ba84c33
Paolo Flores <pflores.fisi22@gmail.com>
2025-04-20 20:57:51 -0500

    document constants

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,11 @@
 use crate::macros::static_assert;
 
+/// The size (in bytes) of a page in the buffer pool
 pub const PAGE_SIZE: usize = 4096;
+
+/// The number of frames in the buffer pool.
+/// The more frames, the more pages we can cache in memory. Increasing this value
+/// will generally improve performance, but will also increase memory usage.
+pub const BUFFER_POOL_N_FRAMES: usize = 69;
 
 static_assert!(PAGE_SIZE % 8 == 0);
